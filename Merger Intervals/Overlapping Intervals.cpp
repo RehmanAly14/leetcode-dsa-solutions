f1c93653705d041d47eaec2e2a9177caeaac01eb
Rehman <sherryrehman250@gmail.com>
2026-02-18 17:10:40 +0500

    Add isIntersect function for interval overlap detection
    Implement isIntersect function to check for overlapping intervals.

---
+++ Merger Intervals/Overlapping Intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool isIntersect(vector<vector<int>> intervals) {
+        // Code Here
+         int start1=intervals[0][0];
+        int end1=intervals[0][1];
+        int n=intervals.size();
+        for(int i=1;i<n;i++){
+            int start2=intervals[i][0];
+            int end2=intervals[i][1];
+            if(end1>=start2){
+               
+                return true;
+            }
+            start1=start1;
+                end1=max(end1,end2);
+        }
+      
+        return false;
+    }
+};
