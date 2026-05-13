6b21ebe5d7d355666880b349d1592ab1418de8d3
Rehman <sherryrehman250@gmail.com>
2026-05-13 22:42:28 +0500

    Implement N-Queens II solution using backtracking

---
+++ Recursion and Backtracking Pattern/N-Queens II.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+bool isSafe(vector<string>&board,int row,int col,int n){
+    //horrizontol
+    for(int j=0;j<n;j++){
+        if(board[row][j]=='Q'){
+            return false;
+        }
+    }
+    //vertical
+     for(int i=0;i<n;i++){
+        if(board[i][col]=='Q'){
+            return false;
+        }
+    }
+    //left diagonal
+    for(int i=row, j=col;j>=0 && i>=0;j--,i--){
+        if(board[i][j]=='Q'){
+            return false;
+        }
+    }
+    //right diagonal
+     for(int i=row,j=col;j<n && i>=0;j++,i--){
+        if(board[i][j]=='Q'){
+            return false;
+        }
+    }
+    return true;
+}
+void nQueens(vector<string>&board,int row,int n,int &ans){
+    if(row==n){
+        ans++;
+        return;
+    }
+    for(int j=0;j<n;j++){
+        if(isSafe(board,row,j,n)){
+            board[row][j]='Q';
+            nQueens(board,row+1,n,ans);
+            board[row][j]='.';
+        }
+    }
+}
+    int totalNQueens(int n) {
+         vector<string>board(n,string(n,'.'));
+        int ans=0;
+        nQueens(board,0,n,ans);
+        return ans; 
+        
+    }
+};
