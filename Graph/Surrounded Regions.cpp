ffc21bb9e9c684cafe68282a4ef3f3cfffdbd314
Rehman <sherryrehman250@gmail.com>
2026-05-15 00:01:35 +0500

    Add solution for Surrounded Regions using DFS
    Implement DFS to solve the Surrounded Regions problem by marking 'O's connected to the border and flipping the remaining 'O's to 'X'.

---
+++ Graph/Surrounded Regions.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+int x[4]={1,-1,0,0};
+int y[4]={0,0,1,-1};
+bool isValid(int i,int j,int n,int m){
+    if(i<0 || i>=n || j<0 || j>=m){
+        return false;
+    }
+    return true;
+}
+void dfs(vector<vector<char>>& a,int n,int m,int i,int j){
+      a[i][j]='#';
+      for(int k=0;k<4;k++){
+        int row=i + x[k];
+        int col=j + y[k];
+        if(isValid(row,col,n,m) && a[row][col]=='O'){
+            dfs(a,n,m,row,col);
+        }
+      }
+      return;
+}
+    void solve(vector<vector<char>>& board) {
+        int n =board.size();
+        int m =board[0].size();
+        //first row
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O'){
+                dfs(board,n,m,0,j);
+            }
+        }
+         //last row
+        for(int j=0;j<m;j++){
+            if(board[n-1][j]=='O'){
+                dfs(board,n,m,n-1,j);
+            }
+        }
+         //first col
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O'){
+                dfs(board,n,m,i,0);
+            }
+        }
+         //last col
+        for(int i=0;i<n;i++){
+            if(board[i][m-1]=='O'){
+                dfs(board,n,m,i,m-1);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='#'){
+                    board[i][j]='O';
+                }
+                else{
+                    board[i][j] ='X';
+                };
+            }
+        }
+        return;
+        
+    }
+};
